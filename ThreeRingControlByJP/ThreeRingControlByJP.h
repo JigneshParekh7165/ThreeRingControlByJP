f70e84c3bb9af6fd388e3e2219cf15d7dea1f17b
Keyur Ashra <vaghelamithun@bitbucket.org>
2017-08-17 18:52:05 +0530

    initial commit by JP

---
+++ ThreeRingControlByJP/ThreeRingControlByJP.h
@@ -0,0 +1,19 @@
+//
+//  ThreeRingControlByJP.h
+//  ThreeRingControlByJP
+//
+//  Created by Keyur Ashra on 17/08/17.
+//  Copyright © 2017 ]!@#. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeRingControlByJP.
+FOUNDATION_EXPORT double ThreeRingControlByJPVersionNumber;
+
+//! Project version string for ThreeRingControlByJP.
+FOUNDATION_EXPORT const unsigned char ThreeRingControlByJPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeRingControlByJP/PublicHeader.h>
+
+
